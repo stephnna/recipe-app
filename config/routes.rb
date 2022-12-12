71988a076988705abb0295fdab512e9723a37126
David-Kasilia <davidkasilia001@gmail.com>
2022-12-12 17:14:08 +0300

    Setup Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  get 'inventory_food/index'
-  get 'food/index'
-  get 'recipe/index'
-  get 'recipe/show'
-  get 'recipes_food/index'
-  get 'inventory/Index'
-  get 'inventory/show'
-  get 'user/index'
+
+  devise_for :user
+
+  root to: 'user#index'
+
+  resources :user
+
+  # get 'inventory_food/index'
+  # get 'food/index'
+  # get 'recipe/index'
+  # get 'recipe/show'
+  # get 'recipes_food/index'
+  # get 'inventory/Index'
+  # get 'inventory/show'
+  # get 'user/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
