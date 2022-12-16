f5bc6cd1d0945e1a3c9120606a80a7aab22adf56
David-Kasilia <davidkasilia001@gmail.com>
2022-12-16 21:51:18 +0300

    Testing and updated readme

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @user = User.create!(name: 'Tom',role: "admin", email: "email@hmail.com", password: "password")
+    Inventory.create!(name: "Inventory 1", description: "my ringtone", user_id: @user.id)
+  end
+
+  it 'should be invalid without name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should be invalid without name' do
+    subject.description = nil
+    expect(subject).to be_valid
+  end
+
 end
